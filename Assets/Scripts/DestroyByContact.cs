61d78ef85c196f1c2e438614f06e69a3df3dff4c
Tom Sell <thomas.j.sell@gmail.com>
2015-02-07 00:10:37 -0500

    added a randomly spinning asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log (other.name);
+		if (other.tag != "Boundary") {
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
